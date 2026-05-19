606fdef30da1e867be53d7477e62b13b91ea4f69
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-05-19 10:10:13 +0530

    OOPS

---
+++ objectOrientedProgramming/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObject1 = new Main();
+    Main myObject2 = new Main();
+
+    System.out.println(myObject1.x);
+    System.out.println(myObject2.x);
+  }
+}
